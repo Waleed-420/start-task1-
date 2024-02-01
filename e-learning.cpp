eb79effc7b87054c12c48c857d31d0c2573d935f
Muhammad Waleed <L1S21BSSE0063@ucp.edu.pk>
2024-02-01 11:48:57 +0500

    added files

---
+++ e-learning.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class e-learning
+{
+      private:
+              int id;
+              string name;
+      public:
+      void setId();
+      void setName();
+      int getId();
+      string getName();
+      };
+      int main()
+      {
+          }
